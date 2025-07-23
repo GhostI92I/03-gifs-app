38dea89416584f8b3fb85b89c497c11ff93008d2
Irán Pérez <iran_phantom@hotmail.com>
2025-07-23 23:41:19 +0100

    Agregar listado de imagenes como componente, e imaegenes como componentes separados

---
+++ src/app/gifs/components/side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface IMenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifsSideMenuOptionsComponent {
+  menuOptions: IMenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Most popular gifs',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search for gifs',
+      route: '/dashboard/search',
+    }
+  ]
+}
